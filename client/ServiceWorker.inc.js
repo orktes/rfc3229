c78a4c36e0b042633fc7c2c1ea3f4b537073eca3
Jaakko Lukkari <jaakko@unity3d.com>
2017-02-08 22:20:41 -0800

    Minor fixes

--- client/ServiceWorker.inc.js
+++ client/ServiceWorker.inc.js
@@ -1,34 +1,35 @@
 'use strict';
 
 
-async function applyPatch (request, response) {
-  if (!response || !response.headers.has('etag')) { return [true, await fetch(request)]; }
+async function applyPatch (request, cachedResponse) {
   const headers = new Headers(request.headers);
-  headers.set('if-none-match', response.headers.get('etag'));
-  headers.set('a-im', 'bsdiff');
-  const patchResponse = await fetch(request.url, { headers });
-  if (
-    patchResponse.status !== 226 ||
-    !patchResponse.headers.has('im') ||
-    patchResponse.headers.get('im') !== 'bsdiff') {
-      if (patchResponse.status === 304) {
-        return [false, response];
-      }
-      return [false, await fetch(request)];
+  if (cachedResponse.headers.has('etag')) {
+    headers.set('if-none-match', cachedResponse.headers.get('etag'));
   }
-  const [{ value: old }, { value: patch }] = await Promise.all([response.body.getReader().read(), patchResponse.body.getReader().read()]);
+
+  headers.set('a-im', 'bsdiff');
+  const serverResponse = await fetch(request.url, { headers });
+
+  if (serverResponse.status !== 226) {
+    return serverResponse.status === 200 ? serverResponse : cachedResponse;
+  }
+
+  const [{ value: old }, { value: patch }] = await Promise.all([
+    cachedResponse.body.getReader().read(),
+    serverResponse.body.getReader().read()
+  ]);
   const newFile = BSDiff.Patch(old, patch);
-  return [true, new Response(newFile[0], {
-    headers: new Headers(patchResponse.headers),
-  })];
+  return new Response(newFile[0], {
+    headers: new Headers(serverResponse.headers),
+  });
 }
 
 async function findAndPatch (request) {
   if (!/foobar/.test(request.url)) { return fetch(request); }
   const cachedResponse = await caches.match(request);
-  const [shouldCacheResponse, response] = await applyPatch(request, cachedResponse);
+  const response = await applyPatch(request, cachedResponse);
   const cache = await caches.open('v1');
-  shouldCacheResponse && cache.put(request, response.clone());
+  cache.put(request, response.clone());
   return response;
 }
 
