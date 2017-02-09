a412118c548b931a26d3637ecabcdd623dd05299
Üstün Ergenoglu <ustun@unity3d.com>
2017-02-09 10:15:23 -0800

    Add parameter parsing for the test client in go

--- patch-client/example/main.go
+++ patch-client/example/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "github.com/orktes/rfc3229/patch-client"
+import (
+	"flag"
+	"net/url"
+
+	"github.com/orktes/rfc3229/patch-client"
+)
 
 func main() {
-	client.Get("http://localhost:8080/foobar", "foobar")
+	urladdress := flag.String("url", "", "URL to fetch")
+	output := flag.String("out", "", "File to write")
+	flag.Parse()
+
+	strout := *output
+	strurladdress := *urladdress
+
+	u, err := url.Parse(strurladdress)
+	if err != nil {
+		panic(err)
+	}
+
+	if strout == "" {
+		strout = u.Path[1:]
+	}
+
+	client.Get(strurladdress, strout)
 }
